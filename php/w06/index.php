3766735393d86ece0688312ec024c064e74ec9c7
FenMin <61653869+FenMin@users.noreply.github.com>
2024-03-25 15:28:04 +0800

    w06

---
+++ php/w06/index.php
@@ -0,0 +1,26 @@
+<style>
+a {
+  /* Set the font family and size for the links */
+  font-family: Arial, sans-serif;
+  font-size: 18px;
+  text-decoration: none; /* Remove the underline from the links */
+  color: #007BFF; /* Set the color of the links */
+  padding: 10px; /* Add some padding around the links */
+  margin: 5px; /* Add some margin between the links */
+  border-radius: 5px; /* Add some rounded corners to the links */
+  transition: background-color 0.3s ease; /* Add a transition effect when hovering over the links */
+}
+
+a:hover {
+  background-color: #007BFF; /* Set the background color of the links when hovering over them */
+  color: #fff; /* Set the color of the links when hovering over them */
+}
+
+</style>
+
+<a href = "sql1.php"> q1 </a>
+<a href = "sql2.php"> q2 </a>
+<a href = "sql3.php"> q3 </a>
+<a href = "sql4.php"> q4 </a>
+<a href = "sql5.php"> q5 </a>
+<a href = "sql6.php"> q6 </a>
---
+++ php/w06/sql2.php
@@ -0,0 +1,40 @@
+<?php
+	include ("configure.php");
+?>
+<!doctype html>
+<html>
+<head>
+	<meta charset="UTF-8" />
+	<title>NTUNHS PHP+MySQL課程範例</title>
+	<meta name="keywords" content="MySQL, PHP, NTUNHS, PHP網路程式設計, 範例程式" />
+	<meta name="description" content="這是「PHP網路程式設計」課程用的PHP+MySQL範例程式" />
+</head>
+
+<body>
+		<?php
+			// 建立與MySQL資料庫的連線
+			$link = new PDO('mysql:host='.$hostname.';dbname='.$database.';charset=utf8mb4', $username, $password);
+
+			// 用SQL語法呼叫exec()
+			$query = "SELECT Nickname , Country from personal where Country = '198%'";
+			$result = $link->query($query);
+			// 若是Insert, Update, Delete，不是使用query()，而是使用exec()
+
+			// $link->exec()的結果會放在$result中；一般為一陣列(Array)，包含N筆資料。
+			// 然後，用$result->fetch()將此N筆資料，1筆1筆的讀出來，放至$row中。
+			//while ($row=$result->fetch_array(MYSQLI_BOTH))	// 原mysqli的讀法
+			//while ($row=$result->fetch(PDO::FETCH_ASSOC))		// 此種方法亦可
+			foreach ($result as $row)							// 同 while ($row = $result->fetch())
+			{
+				// 在此例中，$row中有$row["Time"]與$row["Description"]兩欄位的值。
+				echo $row["Nickname"] . "\t" .$row["Country"] ."<br>";
+			} 
+			
+		
+			echo "<br>" . "<br>" . "總共" . $result -> rowcount() . "筆資料";
+		?>
+
+	</table><br /><br />
+
+</body>
+</html>
